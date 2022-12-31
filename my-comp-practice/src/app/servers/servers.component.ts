4dc85ffbf393ca9330d4815140556dfa7ca25a00
puneet <puneet098@hotmail.com>
2022-12-31 19:54:59 +0530

    assignment

--- my-comp-practice/src/app/servers/servers.component.ts
+++ my-comp-practice/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //selector: '[app-servers]',//it is an attribute
+  //selector: '.app-servers',//by class
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
